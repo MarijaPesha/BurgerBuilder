7885b2620445fe45ea561436ebda76089c5f567a
marija <marija.pesa@verybogthings.com>
2021-04-26 17:58:11 +0200

    Add order page & controller

---
+++ src/app/controllers/order.cljs
@@ -0,0 +1,43 @@
+(ns app.controllers.order
+  (:require [keechma.next.controller :as ctrl]
+            [keechma.next.controllers.pipelines :as pipelines]
+            [keechma.pipelines.core :as pp :refer-macros [pipeline!]]
+            [keechma.next.controllers.form :as form]
+            [app.validators :as v]
+            [clojure.string :as string]
+            [keechma.next.toolbox.logging :as l]
+            [keechma.next.toolbox.ajax :refer [GET POST DELETE PUT]]
+            [app.settings :refer [api-endpoint]]
+            [keechma.next.controllers.router :as router]))
+
+
+(derive :order ::pipelines/controller)
+
+(defn order-create
+  [{:keys[name street zipcode delivery]}]
+  (POST (str api-endpoint "/users/order.json")
+    {:response-format :json, :keywords? true, :format :json
+     :params 
+     {:name name
+      :street street
+      :zipcode zipcode
+      :delivery delivery}}))
+
+(def pipelines
+  {:keechma.on/start order-create
+   :keechma.form/submit-data
+   (pipeline! [value {:keys [meta-state*] :as ctrl}]
+              (let [street (:street value)
+                    zipcode (:zipcode value)
+                    delivery (:delivery value)]
+                (order-create {:street street
+                               :zipcode zipcode
+                               :delivery delivery}))
+              (router/redirect! ctrl :router {:page "home"}))})
+
+(defmethod ctrl/prep :order 
+  [ctrl]
+  (pipelines/register ctrl
+                      (form/wrap pipelines
+                                 (v/to-validator {:street [:not-empty]
+                                                  :zipcode [:not-empty]}))))
---
+++ src/app/ui/pages/order.cljs
@@ -0,0 +1,46 @@
+(ns app.ui.pages.order
+  (:require [helix.dom :as d]
+            [helix.core :as hx :refer [$]]
+            [keechma.next.helix.lib :refer [defnc]]
+            [app.ui.components.inputs :refer [wrapped-input]]
+            [keechma.next.helix.core :refer [with-keechma use-sub dispatch]]
+            [keechma.next.helix.classified :refer [defclassified]]
+            [helix.hooks :as hooks]
+            [keechma.next.controllers.router :as router]
+            [app.ui.components.navbar :refer [Navbar]]
+            [keechma.next.toolbox.logging :as l]))
+
+(defclassified OrderWrapper :div "h-screen w-screen flex items-center")
+(defclassified OrderContainer :div "container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700")
+(defclassified FormWrapper :div "max-w-md")
+(defclassified ButtonType :button "my-16 cursor-pointer bg-white text-sm hover:bg-gray-100 text-gray-600 font-semibold py-2 px-4 border border-gray-400 rounded shadow m-auto")
+
+(defnc OrderRenderer
+  [props]
+  (d/div
+   ($ Navbar)
+   ($ OrderWrapper
+      ($ OrderContainer
+         ($ FormWrapper
+            (d/form
+             {:on-submit (fn [e]
+                           (.preventDefault e)
+                           (dispatch props :order :keechma.form/submit))}
+             (d/p {:className "text-xs font-semibold"} "Please Leave Your Information Below:")
+             (wrapped-input {:keechma.form/controller :order
+                             :input/type :text
+                             :input/attr [:street]
+                             :placeholder "Street"})
+             (wrapped-input {:keechma.form/controller :order
+                             :input/type :text
+                             :input/attr [:zipcode]
+                             :placeholder "Zip Code"})
+             (d/p {:className "text-xs font-semibold mt-6"} "Select Delivery Method:")
+             (wrapped-input {:keechma.form/controller :order
+                             :input/type :select
+                             :input/attr :select
+                             :options [{:value "default" :label "Fastest"}
+                                       {:value "caucasian" :label "Cheapest"}]})
+             ($ ButtonType {:onClick #()} "Confirm Order")))))))
+
+(def Order (with-keechma OrderRenderer))
