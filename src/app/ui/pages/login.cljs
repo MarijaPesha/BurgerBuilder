5a5d2da9a1ac909ab6e67af68d99b06fdcc0a4e1
marija <marija.pesa@verybogthings.com>
2021-04-29 09:18:52 +0200

    Show total price on login page

--- src/app/ui/pages/login.cljs
+++ src/app/ui/pages/login.cljs
@@ -1,12 +1,11 @@
 (ns app.ui.pages.login
-  (:require [keechma.next.helix.core :refer [with-keechma dispatch ]]
+  (:require [keechma.next.helix.core :refer [with-keechma dispatch use-sub]]
             [keechma.next.helix.lib :refer [defnc]]
             [helix.dom :as d]
             ["react" :as react]
             ["react-dom" :as rdom]
             [app.ui.components.inputs :refer [wrapped-input]]
             [keechma.next.controllers.router :as router]
-            ["firebase/app" :as Firebase]
             [keechma.next.helix.classified :refer [defclassified]]
             [helix.core :as hx :refer [$]]))
 
@@ -14,32 +13,35 @@
 (defclassified LoginContainer :div "container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700")
 (defclassified FormWrapper :div "max-w-md")
 (defclassified ButtonWrapper :button "my-3 cursor-pointer bg-white hover:bg-gray-100 text-gray-600 font-semibold py-1 px-4 border border-gray-400 rounded shadow m-auto")
+(defclassified TextWrapper :div "text-green-700 font-semibold py-3 animate-pulse text-2xl")
 
 (defnc LoginRenderer [props]
   ($ LoginWrapper
     ($ LoginContainer
-      ($ FormWrapper 
-          (d/h1
-           {:class "text-green-700 font-semibold py-3 animate-pulse text-2xl"}
-           "Login to add order!")
-          (d/form
-            {:on-submit (fn [e]
-                          (.preventDefault e)
-                          (dispatch props :login :keechma.form/submit))}
-            (wrapped-input {:keechma.form/controller :login,
-                             :input/type :text
-                             :input/attr [:name]
-                             :placeholder "Name"})
-            (wrapped-input {:keechma.form/controller :login,
-                            :input/type :text,
-                            :input/attr :email,
-                            :placeholder "Email"})
-            (wrapped-input {:keechma.form/controller :login,
-                            :input/type :text,
-                            :input/attr [:password],
-                            :type "password",
-                            :placeholder "Password"})
-            ($ ButtonWrapper
-                      "Log In"))))))
+      ($ FormWrapper
+         (let [price (:total-price (get (use-sub props :burgerBuilder) 4))]
+           ($ TextWrapper
+              "The price of your burger is " (.toFixed price 2) "$ !"))
+         ($ TextWrapper
+            "Login to add order.")
+         (d/form
+          {:on-submit (fn [e]
+                        (.preventDefault e)
+                        (dispatch props :login :keechma.form/submit))}
+          (wrapped-input {:keechma.form/controller :login
+                          :input/type :text
+                          :input/attr [:name]
+                          :placeholder "Name"})
+          (wrapped-input {:keechma.form/controller :login
+                          :input/type :text
+                          :input/attr :email
+                          :placeholder "Email"})
+          (wrapped-input {:keechma.form/controller :login
+                          :input/type :text
+                          :input/attr [:password]
+                          :type "password"
+                          :placeholder "Password"})
+          ($ ButtonWrapper
+             "Log In"))))))
 
 (def Login (with-keechma LoginRenderer))
